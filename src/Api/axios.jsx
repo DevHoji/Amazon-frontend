0657c16e5b6d94c222119ab9598623716e8cee1c
yabselam <yabsirahoji@gmail.com>
2024-11-21 12:10:40 -0800

    deployment

--- src/Api/axios.jsx
+++ src/Api/axios.jsx
@@ -1,10 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "http://127.0.0.1:5001/e-clone-9d071/us-central1/api",
-  
-  baseURL: "https://amazon-api-deploy-3-wb8n.onrender.com/",
+  baseURL: "https://us-central1-your-project-id.cloudfunctions.net/api", // Firebase Functions URL
 });
 
-export {axiosInstance}
-
+export { axiosInstance };
